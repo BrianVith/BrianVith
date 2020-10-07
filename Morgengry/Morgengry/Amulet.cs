5491b53e8796a3c672eac1ed5e8b0b992878d0d4
Brian Vith <Brianvithdk@gmail.com>
2020-10-07 11:12:34 +0200

    Morgengry 1

---
+++ Morgengry/Morgengry/Amulet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morgengry
+{
+    public class Amulet
+    {
+        private string itemId;
+        private string design;
+        private Level quality;
+
+        
+
+        public string ItemId
+        {
+            get { return itemId; }
+            set { itemId = value; }
+        }
+
+        public string Design
+        {
+            get { return design; }
+            set { design = value; }
+        }
+
+        public Level Quality
+        {
+            get { return quality; }
+            set { quality = value; }
+        }
+
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            this.itemId = itemId;
+            this.quality = quality;
+            this.design = design;
+        }
+
+        public Amulet(string itemId, Level quality) :
+            this (itemId, quality, "")
+        {
+
+        }
+        
+        public Amulet (string itemId) :
+            this (itemId, Level.medium, "")
+        { 
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {itemId}, Quality: {quality}, Design: {design}";
+        }
+
+    }
+}
---
+++ Morgengry/Morgengry/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morgengry
+{
+    public class Book
+    {
+        private string itemId;
+
+        public string ItemId
+        {
+            get { return itemId; }
+            set { itemId = value; }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public Book(string itemId, string title, double price)
+        {
+            this.itemId = itemId;
+            this.title = title;
+            this.price = price;
+        }
+
+        public Book(string itemid, string title) : 
+            this (itemid, title, 0)
+        {
+        }
+
+        public Book(string itemid) : 
+            this (itemid, "", 0)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {itemId}, Title: {title}, Price: {price}";
+        }
+
+
+
+    }
+}
